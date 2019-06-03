df412911ad72aaaa711c958be24bd61841461daf
Dan Goje <gojedan98@gmail.com>
2019-06-03 09:54:47 +0100

    Add basic test for takeMove function. Fixes #3

--- src/Engine/Moves.hs
+++ src/Engine/Moves.hs
@@ -1,15 +1,21 @@
 module Engine.Moves
     (getMoves
+    ,takeMove
     ,Move
+    ,Color(..)
     ) where
 
 import Data.List
 import Engine.Board
 
 type Move = (Int, Int)
+data Color = Engine | Player
 
 getMoves :: Board -> [Move]
 getMoves b = concat $ zipWith parseRows b [0..]
+
+takeMove :: Board -> Move -> Color -> Board
+takeMove b m c = []
 
 parseRows :: [Square] -> Int -> [Move]
 parseRows r i = map (\(Just x) -> x) $ filter (/= Nothing) $ zipWith (\x j -> if x == Empty then Just (i, j) else Nothing) r [0..]
--- test/Engine/MovesSpec.hs
+++ test/Engine/MovesSpec.hs
@@ -10,9 +10,20 @@ stubMatrix = [
   ["X", "O", ""],
   ["X", "O", ""]]
 
+moveTakenMatrix :: [[String]]
+moveTakenMatrix = [
+  ["O", "X", ""],
+  ["X", "O", ""],
+  ["X", "O", ""]]
+
 stubBoard :: Board
 stubBoard =
   let (Just board) = fromMatrix stubMatrix
+  in board
+
+moveTakenBoard :: Board
+moveTakenBoard =
+  let (Just board) = fromMatrix moveTakenMatrix
   in board
 
 moves :: [Move]
@@ -23,3 +34,6 @@ spec = do
   describe "getMoves" $ do
     it "should return all empty squares on board" $ do
       (getMoves stubBoard) `shouldBe` moves
+  describe "takeMove" $ do
+    it "should return the correct board state when given a valid board state and a valid move" $ do
+      (takeMove stubBoard (moves !! 1) Engine) `shouldBe` moveTakenBoard
