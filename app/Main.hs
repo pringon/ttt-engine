056e7469a57de4c8edd4abe35ed68d4888208249
Dan Goje <gojedan98@gmail.com>
2019-06-22 21:54:17 +0100

    Fix malformed JSON body

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.Char (toLower)
+import Control.Monad
+
 import Network.Wai
 import Network.HTTP.Types (status200, status400, Header)
 import Network.Wai.Handler.Warp (run)
@@ -22,6 +25,20 @@ parseBody rawBody =
     [(b, "")] -> fromMatrix b
     _ -> Nothing
 
+responseBody :: Maybe Square -> Maybe Move -> Bool -> String
+responseBody winner move draw =
+  case winner of
+    (Just square) -> withWinner ("\"" ++ show square ++ "\"") move draw
+    (Nothing) -> withWinner "null" move draw
+  where
+    withWinner :: String -> Maybe Move -> Bool -> String
+    withWinner winner (Just (x, y)) draw = makeString winner (show [x, y]) draw
+    withWinner winner Nothing draw = makeString winner "null" draw
+    makeString :: String -> String -> Bool -> String
+    makeString winner move draw = "{ \"winner\": " ++ winner ++ ", \"move\": " ++ move ++", \"draw\": " ++ boolToJson draw ++ " }"
+    boolToJson :: Bool -> String
+    boolToJson b = Prelude.map toLower $ show b
+
 application :: Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
 application req res = do
   body <- requestBody $ req
@@ -35,15 +52,15 @@ main = run 3000 application
 
 respond :: Board -> (Response -> IO ResponseReceived) -> IO ResponseReceived
 respond b res = do
-  let ((x, y), _) = findBestMove Engine 0 b
+  let (move, _) = findBestMove Engine 0 b
   let body = case getState b of
-        (Won, s) -> "{ winner: " ++ show s ++ ", move: null, draw: false }"
-        (Draw, _) -> "{ winner: null, move: null, draw: true }"
+        (Won, s) -> responseBody (Just s) Nothing False
+        (Draw, _) -> responseBody Nothing Nothing True
         _ ->
-          case getState (takeMove b Engine (x, y)) of
-            (Won, s) -> "{ winner: " ++ show s ++ ", move: " ++ show [x, y] ++ ", draw: false }"
-            (Draw, _) -> "{ winner: null, move: " ++ show [x, y] ++ ", draw: true }"
-            _ -> "{ winner: null, move: " ++ show [x, y] ++ ", draw: false }"
+          case getState (takeMove b Engine move) of
+            (Won, s) -> responseBody (Just s) (Just move) False
+            (Draw, _) -> responseBody Nothing (Just move) True
+            _ -> responseBody Nothing (Just move) False
   res $ responseLBS status200 defaultHeaders $ BL8.pack body
 
 handleError :: (Response -> IO ResponseReceived) -> IO ResponseReceived
